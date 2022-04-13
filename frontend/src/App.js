eef43ff8c54ef675fc66a61cbfc3a9b5f3ec9671
kirkhang <kirkzhang.yu@gmail.com>
2022-04-13 20:34:22 +1200

    UseState and UseEffect

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,21 @@
-import { Button } from "antd";
+import { useState, useEffect } from 'react'
 import { getAllStaff } from './client.js'
 import './App.css';
 
 function App() {
-  getAllStaff()
-    .then(res => res.json())
-    .then(console.log)
-  return "React"
+  const [staff, setStaff] = useState([]);
+
+  const fetchStaff = () =>
+    getAllStaff()
+      .then(res => res.json())
+      .then(console.log)
+
+  useEffect(() => {
+    console.log("aaaaaaaaaaaaaaaaaaa");
+    fetchStaff();
+  }, []);
+
+  return <p>{staff.length}</p>
 }
 
 export default App;
